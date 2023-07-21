8576846df11fa0ded7d604323cbeb79de81727c5
Serge <serge.vasylechko@tch.harvard.edu>
2023-07-21 11:55:26 -0400

    example how to run

---
+++ example.py
@@ -0,0 +1,8 @@
+# Add the path of the directory containing proxy_setup.py to your Python path
+import sys
+sys.path.append("/path/to/directory/containing/proxy.py")
+
+# Now you can import and use the setup_proxy function
+from proxy import setup_proxy
+
+setup_proxy()
